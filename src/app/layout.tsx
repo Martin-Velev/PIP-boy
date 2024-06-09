6f1edbc37e62be6e2351a25063860a27b5bd068f
Jojo <jojo.nedich@gmail.com>
2024-06-10 01:48:41 +0300

    fix css import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import RootNav from '@/components/nav/rootNav'
 import { CharProvider } from '@/providers/CharProvider'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
